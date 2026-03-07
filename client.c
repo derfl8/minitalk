de254cd5f77f3b7effdc4d23da7a0b396b6c29ca
derfl8 <alfred.begou@gmail.com>
2026-03-07 13:56:57 +0100

    1st try to read a oct

--- client.c
+++ client.c
@@ -6,19 +6,34 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 18:24:32 by abegou            #+#    #+#             */
-/*   Updated: 2026/03/05 16:48:47 by abegou           ###   ########.fr       */
+/*   Updated: 2026/03/06 18:27:08 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	send(unsigned int pid, char c)
+{
+	unsigned int	oct;
+	
+	oct = 7;
+	while (oct >= 0)
+	{
+		if ((c >> oct) & 1)
+			kill(pid, SIGUSR1);
+		else //if ((c >> oct) & 0)
+			kill(pid, SIGUSR2);
+		usleep(100);
+		oct--;
+	}
+}
+
 int	main(int ac, char **av)
 {
-	unsigned int	pid;
-
 	if (ac != 3)
 		return (1);
-	pid = ft_atoi(av[1]);
-	ft_printf("%u", pid);
+	else if (ft_atoi(av[1]) < 1)
+		ft_printf("%s", "Wrong PID dumbass");
+	send(ft_atoi(av[1]), 'a');
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,13 +6,44 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 18:24:40 by abegou            #+#    #+#             */
-/*   Updated: 2026/03/05 18:04:08 by abegou           ###   ########.fr       */
+/*   Updated: 2026/03/06 18:18:39 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf/ft_printf.h"
 #include "minitalk.h"
+
+static void	handler(int sig)
+{
+	unsigned char	c;
+	unsigned int	oct;
+
+	c = 0;
+	oct = 0;
+	if (sig == SIGUSR2)
+		c |= (1 << (7 - oct));
+//	else if (sig == SIGUSR2)
+//		c |= (0 << (7 - oct));
+	oct++;
+	if (oct == 8)
+	{
+		ft_printf("%c", c);
+		c = 0;
+		oct = 0;
+	}
+}
 
 int	main(void)
 {
+	struct sigaction	sa;
+
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	sa.sa_flags = SA_SIGINFO;
 	ft_printf("%d", getpid());
+	while(1)
+		pause();
 }
